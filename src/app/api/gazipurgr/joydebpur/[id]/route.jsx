87bb518727cd1dd0d3591b5c4ac849adbec527f2
Anisur369 <anisurrahman57136@gmail.com>
2026-04-19 23:37:08 +0600

    api backend id

---
+++ src/app/api/gazipurgr/joydebpur/[id]/route.jsx
@@ -0,0 +1,59 @@
+import { ObjectId } from "mongodb";
+// import { connect } from "../../lib/dbConnect";
+import { connect } from "../../../lib/dbConnect2";
+const allCaseCollection = connect("joydebpur");
+
+export async function GET(request, { params }) {
+    const { id } = await params;
+    const singleAllCase = await allCaseCollection.findOne({ _id:new ObjectId(id)});
+    if(!singleAllCase) {
+        return Response.json({ 
+            status:404,
+            message: 'All case not found' });
+    }
+    return Response.json(singleAllCase);
+}
+
+
+export async function PUT(request, { params }) {
+  try {
+    const { id } = await params;
+    const body = await request.json();
+
+    delete body._id; // ✅ prevent error
+    
+    const result = await allCaseCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: body }
+    );
+    console.log(id);
+
+    if (result.matchedCount === 0) {
+      return Response.json({ status: 404, message: "Case not found" });
+    }
+
+    const updatedData = await allCaseCollection.findOne({
+      _id: new ObjectId(id),
+    });
+
+    return Response.json(updatedData);
+  } catch (error) {
+    return Response.json({
+      status: 500,
+      message: "Update failed",
+    });
+  }
+}
+
+
+export async function DELETE(request, { params }) {
+    const { id } = await params;
+    const deleteAllCaseData = await allCaseCollection.deleteOne({ _id: new ObjectId(id) });
+    if (deleteAllCaseData.deletedCount === 0) {
+        return Response.json({
+            status: 404,
+            message: 'All case not found'
+        });
+    }
+    return Response.json(deleteAllCaseData);
+}
